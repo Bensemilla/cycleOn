2cd5ec9a47b0ad9a670598a32daea66aa51a40c8
Sara <sara.brandt@web.de>
2023-08-23 20:52:55 +0200

    meeting updates

---
+++ frontend/pages/aboutCycleOn.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Kosugi_Maru } from "next/font/google";
+
+export default function aboutUs() {
+  return (
+    <>
+      <Head>
+        <title>cycleOn</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
