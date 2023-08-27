d8cd130501b3de8686f7f718ddfad6a7eb396beb
CarinaBr <c.bm0109@gmail.com>
2023-08-27 16:15:56 +0200

    created GET, POST, PUT, DELETE endpoint for rating route

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Rating = require("./models/ratingModel");
 
 const appExpress = express();
 
@@ -21,6 +22,22 @@ appExpress.post("/rating", (req, res) => {
   console.log(req.body);
   res.send(req.body); // whenever I hit the endpoint /rating, this logic will be executed. Routes are equivalent to URL in browser
 });
+/* mit Medium Tutorial (kann man autom. email aus user profile ziehen, um Rating in DB direkt mit user zu verknüpfen?):
+
+appExpress.post('/rating', async (req, res) => {
+  const { roadname, rating, comments } = req.body;
+
+  try {
+    const Rating = new Rating({ roadname, rating, comments });
+    await Rating.save();
+    res.send(Rating);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+*/
 
 // starting the app on port 3000
 appExpress.listen(3000, (err) => {
