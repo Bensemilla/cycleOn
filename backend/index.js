2a6ddfb3002e85cb8a33f8340dee2affad724b43
Benjamin Schroeder <benjamin.schroeder587@gmail.com>
2023-08-27 14:56:04 +0200

    created user login route and added additional check for username at registration route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const parser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 const appExpress = express();
 appExpress.use(parser.json());
@@ -32,19 +33,52 @@ appExpress.listen(3000, (err) => {
 appExpress.post("/register", (req, res) => {
   //Check if email already registered
 
-  User.findOne({ email: req.body.email }).then((user) => {
+  User.findOne({
+    $or: [{ email: req.body.email }, { userName: req.body.userName }],
+  }).then((user) => {
     if (user) {
       // respond with a 400 error if email already exists
-      return res.status(400).json({ email: "email already registered" });
-    } else {
+      return res
+        .status(400)
+        .json({ email: "email already registered or username already in use" });
+    }
+    // !! -------- check for username here ----------- !!
+    else {
       //create new user
       const newUser = new User({
+        name: req.body.name,
         userName: req.body.userName,
         email: req.body.email,
         password: req.body.password,
+        age: req.body.age,
+        bikeType: req.body.bikeType,
       });
       newUser.save();
       return res.status(200).json({ msg: newUser });
     }
   });
+});
+
+// ---------- USER LOGIN: ------------
+
+appExpress.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw Error("User not found");
+  }
+  if (password === user.password) {
+    const token = jwt.sign({ user }, "yourSecretKey", {
+      expiresIn: "24h",
+    });
+
+    res.json({
+      user,
+      token,
+      message: "user logged in successfully",
+    });
+  } else {
+    res.status(401).json({ msg: "unauthenticated" });
+  }
 });
