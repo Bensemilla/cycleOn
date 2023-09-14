23f4167276ad041d776d09c80b5ab6f4869d4ae8
CarinaBr <c.bm0109@gmail.com>
2023-09-14 20:32:29 +0200

    dynamic variables for frontend form, connection backend to frontend user registration, defines frontend running on port 2500

--- frontend/components/registrationbox.js
+++ frontend/components/registrationbox.js
@@ -1,12 +1,37 @@
 import { useState } from "react";
+import React from "react";
 export default function Registrationbox() {
   const [tab, setTab] = useState("register");
+
   function showSignupTab() {
     setTab("register");
   }
   function showLoginTab() {
     setTab("login");
   }
+
+  // defining dynamic variables for input
+  const [name, setName] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [secret, setSecret] = React.useState("");
+  const [secretver, setSecretver] = React.useState("");
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3000/register", {
+      method: "POST",
+      body: JSON.stringify({
+        userName: username,
+        name,
+        email,
+        password: secret,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+  };
+
   return (
     <div className="container-box">
       <div>
@@ -15,35 +40,47 @@ export default function Registrationbox() {
       </div>
       {tab === "register" ? (
         <div className="register">
-          <form>
-            <label for="name">How should we call you?</label>
+          <form onSubmit={submit}>
+            <label for="name-field">How should we call you?</label>
             <br />
             <input
               type="text"
-              id="name"
+              id="name-field"
               name="name"
-              value="Your name"
+              value={name}
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
               required
             ></input>
             <br />
             <br />
-            <label for="user">Please enter a username:</label>
+            <label for="user-field">Please enter a username:</label>
             <br />
             <input
               type="text"
-              id="user"
+              id="user-field"
               name="username"
-              value="Your username"
+              value={username}
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
               required
             ></input>
             <br />
             <br />
-            <label for="email">Please enter your email address:</label> <br />
+            <label for="email-field">
+              Please enter your email address:
+            </label>{" "}
+            <br />
             <input
               type="email"
-              id="email"
+              id="email-field"
               name="email"
-              value=" Your email address"
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
               required
             ></input>
             <br />
@@ -68,12 +105,36 @@ export default function Registrationbox() {
                 {/* How can I let the number for the age start at a certain level, i.e. 15 years ? */}
             <br />
             <br />
-            <label for="pw">Please choose a password</label> <br />
-            <input type="password" id="pw" name="password" required></input>
+            <label for="pw-field">Please choose a password</label> <br />
+            <input
+              type="password"
+              id="pw-field"
+              name="password"
+              value={secret}
+              onChange={(event) => {
+                setSecret(event.target.value);
+              }}
+              required
+            />
             <br />
             <br />
-            <label for="pw">Please verify your password</label> <br />
-            <input type="password" id="pw" name="password" required></input>
+            <label for="pw-verification-field">
+              Please verify your password
+            </label>
+            <br />
+            <input
+              type="password"
+              id="pw-verification-field"
+              name="password"
+              value={secretver}
+              onChange={(event) => {
+                setSecretver(event.target.value);
+              }}
+              required
+            />
+            {/* CHECK IF pw-field & pw-verification-field have the same content */}
+            {/* need help with if function here*/}
+            conditional rendering https://react.dev/learn/conditional-rendering
             <br />
             <br />
             <br />
