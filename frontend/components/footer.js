3ea5351c501e3327f77dfeb66bc5f2e136abe35c
Sara <sara.brandt@web.de>
2023-09-16 13:36:30 +0200

    minor changes

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -1,6 +1,7 @@
 export default function footer() {
   return (
     <div className="footer">
+      <br></br>
       <p>
         This WebApp was created as a project within the
         <a href="https://techlabs.org/">Digital Shaper Program</a> offered by
