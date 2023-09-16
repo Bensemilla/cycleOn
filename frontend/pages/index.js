3182ba1409d7e14c901fc0cddf8bae725be53c8c
brunomoschetto <bruno.moschetto@gmail.com>
2023-09-16 15:28:07 +0200

    map in the index file

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,6 +3,7 @@ import Button from "@/components/button";
 import Registrationbox from "@/components/registrationbox";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import Map from "@/components/map";
 import { useState } from "react";
 
 export default function Home() {
@@ -18,35 +19,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main>
         <Navbar />
-        <div className="content">
-          <div className="map">
-            <br></br>
-            <br></br>
-            <a>place map here</a>
-            <br></br>
-            <br></br>
-          </div>
 
-          <div className="container-map">
-            <h1 className="header-map">Embed Location Map</h1>
-            <iframe
-              className="map-embebed"
-              src="https://experience.arcgis.com/experience/6c0145e31c4b48358864525acdf72fe8"
-              width="600"
-              height="450"
-              style={{ border: 0 }}
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </div>
-
-          <Button onClick={boxAppear} />
-          {showBox === true ? <Registrationbox /> : null}
-        </div>
+        <Map />
       </main>
+
       <Footer />
     </>
   );
