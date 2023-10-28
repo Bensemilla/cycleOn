cdfb973de1a5865b7a9d6c26da7cb6ab8c967cc0
Benjamin Schroeder <benjamin.schroeder587@gmail.com>
2023-10-28 20:39:14 +0200

    added comments for clarification in ratings route file

--- backend/routes/ratings.js
+++ backend/routes/ratings.js
@@ -13,13 +13,15 @@ deleteRating .delete
 */
 
 // CREATE /rating
-
 router.post("/", JWTAuth, ratingsController.createRating);
 
+//UPDATE /rating
 router.put("/", JWTAuth, ratingsController.updateRating);
 
+//SHOW ALL /rating
 router.get("/", ratingsController.showRatings);
 
+//DELETE /rating
 router.delete("/", JWTAuth, ratingsController.deleteRating);
 
 module.exports = router;
