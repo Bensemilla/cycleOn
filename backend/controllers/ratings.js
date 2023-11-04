0334c0e2e4585f31496decd757bf828a4e9469fc
Benjamin Schroeder <benjamin.schroeder587@gmail.com>
2023-11-04 16:32:17 +0100

    uniformized promises in ratings controller file

--- backend/controllers/ratings.js
+++ backend/controllers/ratings.js
@@ -4,31 +4,30 @@ const User = require("../models/User");
 //Create a Rating
 
 const createRating = async (req, res) => {
-  Rating.findOne({
+  const userRating = await Rating.findOne({
     roadname: req.body.roadname,
     ratingFromUser: req.user.user.userName,
-  }).then((userRating) => {
-    if (userRating) {
-      res.status(500).json({ msg: "user already rated street" });
-      console.log("user already rated street");
-      return;
-    } else {
-      const { roadname, rating, comments, ratingFromUser } = req.body;
-      try {
-        const Rating1 = new Rating({
-          roadname,
-          rating,
-          comments,
-          ratingFromUser: req.user.user.userName,
-        });
-        Rating1.save(); // rating.save wird autom. asynchron geladen (was autom. asynchron ist findet man in der jwlg. Doku vom Package, hier: Doku von Mongoose) & mit await mache ich es wieder synchron
-        res.send(Rating1);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send(error);
-      }
-    }
   });
+  if (userRating) {
+    res.status(500).json({ msg: "user already rated street" });
+    console.log("user already rated street");
+    return;
+  } else {
+    const { roadname, rating, comments } = req.body;
+    try {
+      const Rating1 = new Rating({
+        roadname,
+        rating,
+        comments,
+        ratingFromUser: req.user.user.userName,
+      });
+      await Rating1.save(); // rating.save wird autom. asynchron geladen (was autom. asynchron ist findet man in der jwlg. Doku vom Package, hier: Doku von Mongoose) & mit await mache ich es wieder synchron
+      res.send(Rating1);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  }
 };
 
 //update a Rating
