a05e0a129f2ab2fdabc37e41e5546eee0110cfbf
Benjamin Schroeder <benjamin.schroeder587@gmail.com>
2023-11-13 17:15:50 +0100

    added santization to user and ratings validation middlewares

--- backend/middleware/ratingDataValidation.js
+++ backend/middleware/ratingDataValidation.js
@@ -1,21 +1,29 @@
 const { body } = require("express-validator");
 
+//------ Middleware validates and at the same time sanitizes the input data -----------
+
 const ratingDataValidation = [
   body("roadname")
     .exists()
     .withMessage("Roadname required")
     .isString()
-    .withMessage("Name must be a string"),
+    .withMessage("Name must be a string")
+    .escape(),
   body("rating")
     .exists()
     .withMessage("Rating is required")
     .isInt()
-    .withMessage("Rating must be an integer"),
+    .withMessage("Rating must be an integer")
+    .escape(),
   body("comments")
     .exists()
+    .isLength({ max: 100 })
+    .withMessage("Maximum 100 chracters allowed")
     .optional()
     .isString()
-    .withMessage("Comment must be a string"),
+    .withMessage("Comment must be a string")
+    .trim()
+    .escape(),
 ];
 
 module.exports = ratingDataValidation;
--- backend/middleware/userDataValidation.js
+++ backend/middleware/userDataValidation.js
@@ -1,32 +1,49 @@
 const { body } = require("express-validator");
 
+//------ Middleware validates and at the same time sanitizes the input data -----------
+
 const userDataValidation = [
   body("name")
     .exists({ checkFalsy: true })
     .withMessage("Please enter a name")
     .isString()
-    .withMessage("Name must be a string"),
+    .withMessage("Name must be a string")
+    .trim()
+    .escape(),
   body("userName")
     .exists({ checkFalsy: true })
     .withMessage("User name is required")
     .isString()
-    .withMessage("User name must be a string"),
-  body("email").isEmail().withMessage("Provide a valid email address"),
+    .withMessage("User name must be a string")
+    .trim()
+    .escape(),
+  body("email")
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("Provide a valid email address")
+    .trim()
+    .escape(),
   body("password")
     .exists()
     .withMessage("Password is required")
     .isString()
     .withMessage("Password must be a string")
     .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters"),
+    .withMessage("Password must be at least 6 characters")
+    .trim()
+    .escape(),
   body("dateOfBirth")
     .optional()
     .isDate()
-    .withMessage("Please enter a valid date"),
+    .withMessage("Please enter a valid date")
+    .trim()
+    .escape(),
   body("bikeType")
     .optional()
     .isString()
-    .withMessage("Bike type must be a string"),
+    .withMessage("Bike type must be a string")
+    .trim()
+    .escape(),
 ];
 
 module.exports = userDataValidation;
